a7f94cbae9d92fc68511e4b087aae560bac1a442
aniketdd26 <61134732+aniketdd26@users.noreply.github.com>
2021-10-23 17:51:30 +0530

    Code skeleton

---
+++ src/main/java/damnewletter/DamNewsletterApplication.java
@@ -0,0 +1,13 @@
+package damnewletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamNewsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamNewsletterApplication.class, args);
+	}
+
+}
